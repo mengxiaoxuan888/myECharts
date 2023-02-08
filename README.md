b8731871305fe4801a2420e63d4ea73df5562202
mengxiaoxuan888 <96916505+mengxiaoxuan888@users.noreply.github.com>
2023-02-08 03:02:25 +0000

    第二次提交：使用nodejs+express搭建后端环境

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # myECharts
 使用ECharts+VUE3.0实现数据可视化
+
+# myapp
+该文件夹下是搭建的前端项目
+
+# server
+该文件夹下是搭建的后端项目,采用nodejd+express
---
+++ server/router/chartsFour.js
@@ -0,0 +1,13 @@
+// 引入express
+const express = require("express");
+// 创建路由
+const router = express.Router();
+
+// 获取请求,当发送的是get请求时,并且链接到/user
+router.get("/user", (req, res) => {
+  // 发送json数据返回给请求者
+  res.send('{"name":"mengxiaoxuan","age":18}');
+});
+
+// 将路由暴漏出去
+module.exports = router;
